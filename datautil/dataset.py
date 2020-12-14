6be4f39e79c773303f66900f9d96db13563bea20
stdio2016 <randomfrank8@gmail.com>
2020-12-14 23:22:40 +0800

    rewrite dataset loader for performance

--- datautil/dataset.py
+++ datautil/dataset.py
@@ -4,14 +4,18 @@ from pathlib import Path
 import time
 import os
 import warnings
+import struct
 
 import tqdm
 from simpleutils import get_hash
 
 import torch
+import torch.fft
 import torch.multiprocessing as mp
+import torch.nn.functional as F
 import numpy as np
 import miniaudio
+import scipy.signal
 # torchaudio currently (0.7) will throw warning that cannot be disabled
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
@@ -20,32 +24,55 @@ from datautil.audio import get_audio
 torchaudio.USE_SOUNDFILE_LEGACY_INTERFACE = False
 
 class MyDataset(torch.utils.data.Dataset):
-    def __init__(self, path, cache_dir='caches', hop_size=0.5, clip_size=1.2, sample_rate=44100):
+    def __init__(self, path, cache_dir='caches', hop_size=0.5, clip_size=1.2, sample_rate=8000, clips_per_song=60, sel_size=1, pad_start=1):
         super(MyDataset, self).__init__()
-        self.cache_dir = Path(cache_dir)
-        self.cache_dir.mkdir(parents=True, exist_ok=True)
-        self.clip_size = 1.2
+        self.clip_size = int(clip_size * sample_rate)
+        self.sel_size = int(sel_size * sample_rate)
+        self.hop_size = int(hop_size * sample_rate)
+        self.pad_start = int(pad_start * sample_rate)
         self.sample_rate = sample_rate
+        self.clips_per_song = clips_per_song
         with open(path, 'r', encoding='utf8') as fin:
             reader = csv.DictReader(fin)
-            files = list(reader)
-        self.files = []
-        for file in files:
-            name = file['file']
-            hash = get_hash(name)
-            du = float(file['duration'])
+            self.files = [f['file'] for f in reader]
+        self.prepare_cache(cache_dir, self.files)
+    
+    def prepare_cache(self, cache_dir, files):
+        self.cache_dir = Path(cache_dir)
+        self.cache_dir.mkdir(parents=True, exist_ok=True)
+        
+        self.durations = torch.zeros(len(files), dtype=torch.int)
+        pool = mp.Pool(4)
+        songPos = [None] * len(files)
+        nsegs = 0
+        for i, usable in tqdm.tqdm(pool.imap_unordered(self.load_from_hdd, enumerate(files)), total=len(files)):
+            songPos[i] = usable
+            nsegs += len(usable)
+        
+        # collect songs segmented by 30sec
+        partToSong = []
+        partPos = []
+        n = len(self.files)
+        songToPart = np.zeros(n+1, dtype=np.int)
+        for i in range(n):
             t = 0
-            # remove "lock" files
-            if (self.cache_dir / (hash+'_')).exists():
-                (self.cache_dir / (hash+'_')).unlink()
-            while t + clip_size <= du:
-                self.files.append({
-                    'file': name,
-                    'duration': du,
-                    'start': t,
-                    'hash': hash
-                })
-                t += hop_size
+            while t + self.clip_size <= self.durations[i]:
+                partToSong.append(i)
+                partPos.append(t)
+                t += self.clips_per_song * self.hop_size
+            songToPart[i+1] = len(partToSong)
+        self.partToSong = torch.tensor(partToSong, dtype=torch.int)
+        self.partPos = torch.tensor(partPos, dtype=torch.int)
+        
+        # collect songs segmented by 1sec
+        self.segToPart = np.repeat(songToPart[0:-1], [len(x) for x in songPos])
+        self.segPos = np.concatenate(songPos)
+        self.segToPart += self.segPos // (self.clips_per_song * self.hop_size)
+        self.partToSeg = np.concatenate([[0], np.cumsum(np.bincount(self.segToPart))])
+        
+        self.segToPart = torch.tensor(self.segToPart)
+        self.segPos = torch.tensor(self.segPos)
+        self.partToSeg = torch.tensor(self.partToSeg)
     
     def load_from_cache(self, hash, frame_offset, num_frames):
         with open(self.cache_dir / hash, 'rb') as fin:
@@ -56,7 +83,18 @@ class MyDataset(torch.utils.data.Dataset):
         wave /= 32768
         return torch.FloatTensor(wave.reshape([1, -1]))
     
-    def load_from_hdd(self, name, hash):
+    def load_from_hdd(self, i_name):
+        i, name = i_name
+        hash = get_hash(name)
+        (self.cache_dir/hash[0:2]).mkdir(exist_ok=True)
+        cachepath = self.cache_dir/hash[0:2]/hash[2:]
+        if cachepath.exists():
+            with open(cachepath, 'rb') as fout:
+                duration = np.frombuffer(fout.read(4), dtype=np.int32)[0]
+                self.durations[i] = duration
+                fout.seek(4 + duration * 2)
+                usable = np.frombuffer(fout.read(), dtype=np.int32)
+            return i, usable
         wave, smpRate = get_audio(name)
         wave = torch.FloatTensor(wave)
         # stereo to mono
@@ -64,57 +102,69 @@ class MyDataset(torch.utils.data.Dataset):
         # resample to 44100
         torch.set_num_threads(1)
         wave = torchaudio.compliance.kaldi.resample_waveform(wave, smpRate, self.sample_rate)
-        saves = wave.numpy()
+        duration = int(wave.shape[1])
+        # find usable segments
+        t = 0
+        vol = float(torch.max(torch.abs(wave)))
+        usable = []
+        while t + self.clip_size < duration:
+            part = wave[:, t:t+self.clip_size]
+            if float(torch.max(torch.abs(part))) > vol * 1e-4:
+                usable.append(t)
+            else:
+                print('%s frame %f dropped' % (name, t/self.sample_rate))
+            t += self.hop_size
+        usable = np.array(usable, dtype=np.int32)
         # float32 to int16
-        saves = np.clip(saves * 32768, -32768, 32767).astype(np.int16)
-        # save to temporary location
-        tmppath = self.cache_dir / ('_'+str(os.getpid()))
-        saves.tofile(tmppath)
+        torch.clamp(wave * 32768, -32768, 32767, out=wave)
+        wave = wave.type(torch.int16)
+        self.durations[i] = duration
         # save to cache
-        try:
-            tmppath.rename(self.cache_dir / hash)
-        except FileExistsError: # can only happen on Windows
-            tmppath.unlink()
-        except PermissionError: # can only happen on Windows
-            pass
-        return torch.FloatTensor(saves) / 32768
+        wave = wave.numpy().flatten()
+        with open(cachepath, 'wb') as fout:
+            fout.write(np.array([duration], dtype=np.int32))
+            fout.write(wave)
+            fout.write(usable)
+        return i, usable
     
     def __getitem__(self, index):
         #print('I am %d and I have %d' % (os.getpid(), index))
-        file = self.files[index]
-        hash = file['hash']
-        t_start = int(file['start'] * self.sample_rate)
-        t_duration = int(self.clip_size * self.sample_rate)
-        lock_file = self.cache_dir / (hash+'_')
-        # wait for other workers to finish
-        if lock_file.exists():
-            while not (self.cache_dir / hash).exists():
-                time.sleep(0.01)
-        if (self.cache_dir / hash).exists():
-            # cache hit!
-            return self.load_from_cache(hash, t_start, t_duration)
-        # create "lock" file
-        with open(lock_file, 'wb'):
-            pass
-        
-        name = file['file']
-        wave = self.load_from_hdd(name, hash)
-        try:
-            lock_file.unlink()
-        except (PermissionError, FileNotFoundError):
-            pass
-        return wave[:, t_start:t_start+t_duration]
+        wave, pad_start, start, du = index
+        wave = wave[start-pad_start:start+du].to(torch.float32)
+        wave *= 1/32768
+        pos = torch.randint(0, du-self.sel_size, size=(1,))
+        wav1 = wave[pad_start:pad_start+self.sel_size]
+        wav2 = wave[max(0, pad_start+pos-self.pad_start) : pad_start+self.sel_size+pos]
+        wav2 = F.pad(wav2, (self.pad_start+du-len(wav2), 0))
+        return (wav1, wav2)
     def __len__(self):
-        return len(self.files)
+        return len(self.segToPart)
 
-def preloader_func(dir, que):
-    while True:
-        file = que.get()
-        if file == '':
-            break
-        if (dir/file).exists():
+def preloader_func(dir, in_que, out_que, pad_start):
+    loadlist = in_que.get()
+    n_chunk = 0
+    while loadlist is not None:
+        # get total size
+        total = 0
+        for file, start, du in loadlist:
+            total += start - max(start - pad_start, 0) + du
+        dats = np.zeros(total, dtype=np.int16)
+        # load file to a big numpy array
+        total = 0
+        posinfo = []
+        for file, start, du in loadlist:
+            prestart = max(start - pad_start, 0)
+            paddu = (start-prestart) + du
             with open(dir / file, 'rb') as fin:
-                preloaded = fin.read()
+                fin.seek(4 + prestart*2)
+                preloaded = np.frombuffer(fin.read(paddu*2), dtype=np.int16)
+            #print(paddu, len(preloaded))
+            dats[total:total+paddu] = preloaded
+            posinfo.append({'padStart':start-prestart, 'start':total+(start-prestart), 'duration':du})
+            total += paddu
+        out_que.put((torch.tensor(dats), posinfo))
+        loadlist = in_que.get()
+        n_chunk += 1
 
 class MySampler(torch.utils.data.Sampler):
     def __init__(self, data_source, chunk_size):
@@ -122,38 +172,77 @@ class MySampler(torch.utils.data.Sampler):
         self.chunk_size = chunk_size
         self.generator = torch.Generator()
         self.generator2 = torch.Generator()
+        self.pad_start = data_source.pad_start
+    
+    def _preload(self, shuffled, start, que):
+        dataset = self.data_source
+        cnt = 0
+        end = start
+        while end < len(shuffled) and cnt < self.chunk_size:
+            idx = shuffled[end]
+            cnt += int(dataset.partToSeg[idx+1] - dataset.partToSeg[idx])
+            end += 1
+        loadlist = []
+        for partId in shuffled[start:end]:
+            file = dataset.files[dataset.partToSong[partId]]
+            hash = get_hash(file)
+            hash = hash[0:2]+'/'+hash[2:]
+            total = int(dataset.durations[dataset.partToSong[partId]])
+            startTime = int(dataset.partPos[partId])
+            duration = min(total-startTime, (dataset.clips_per_song-1)*dataset.hop_size + dataset.clip_size)
+            loadlist.append((hash, startTime, duration))
+        que.put(loadlist)
+        return end, cnt
+    
     def __iter__(self):
-        que = mp.Manager().Queue()
-        preloader = mp.Process(target=preloader_func, args=(self.data_source.cache_dir, que,))
-        n = len(self.data_source)
-        bs = self.chunk_size
-        n_chunks = (n-1) // bs + 1
+        in_que = mp.Queue()
+        out_que = mp.Queue()
+        preloader = mp.Process(target=preloader_func, args=(self.data_source.cache_dir, in_que, out_que, self.pad_start))
+        dataset = self.data_source
+        n = len(dataset.partToSong)
         preloader.start()
-        shuffled = torch.randperm(n_chunks, generator=self.generator).tolist()
-        # send file names to preloader
-        which = set()
-        i = shuffled[0]
-        for j in range(i*bs, min((i+1)*bs, n)):
-            file = self.data_source.files[j]
-            if file['hash'] not in which:
-                which.add(file['hash'])
-                que.put(file['hash'])
-        for i in range(len(shuffled)):
-            start = shuffled[i] * bs
-            size = min(bs, n - start)
-            if i+1 < len(shuffled):
+        shuffled = torch.randperm(n, generator=self.generator).tolist()
+        chunkpos = 0
+        next_chunkpos, cnt = self._preload(shuffled, chunkpos, in_que)
+        while chunkpos < n:
+            if next_chunkpos < n:
                 # send file names to preloader
-                for j in range(shuffled[i+1]*bs, min((shuffled[i+1]+1)*bs, n)):
-                    file = self.data_source.files[j]
-                    if file['hash'] not in which:
-                        which.add(file['hash'])
-                        que.put(file['hash'])
-            yield from (start + torch.randperm(size, generator=self.generator2)).tolist()
+                next_chunkpos2, cnt2 = self._preload(shuffled, next_chunkpos, in_que)
+            # shuffle current chunk
+            segIds = []
+            for partId in shuffled[chunkpos : next_chunkpos]:
+                segIds.append(torch.arange(dataset.partToSeg[partId], dataset.partToSeg[partId+1]))
+            segIds = torch.cat(segIds)
+            loaded, posinfo = out_que.get()
+            posdict = {}
+            for i, info in enumerate(posinfo):
+                partId = shuffled[chunkpos+i]
+                posdict[partId] = info
+            for i in torch.randperm(len(segIds), generator=self.generator2).tolist():
+                # look up for segment position
+                segId = int(segIds[i])
+                partId = int(dataset.segToPart[segId])
+                partPos = int(dataset.partPos[partId])
+                segPos = int(dataset.segPos[segId])
+                padInfo = posdict[partId]
+                seg_start = padInfo['start'] + (segPos - partPos)
+                seg_padStart = min(self.pad_start, padInfo['padStart'])
+                seg_du = dataset.clip_size
+                yield (loaded, seg_padStart, seg_start, seg_du)
+            chunkpos = next_chunkpos
+            if next_chunkpos < n:
+                next_chunkpos = next_chunkpos2
+                cnt = cnt2
         # stop preloader
-        que.put('')
+        in_que.put(None)
         preloader.join()
+    
     def __len__(self):
         return len(self.data_source)
+    
+    def set_epoch(self, epoch):
+        self.generator.manual_seed(42 + epoch)
+        self.generator2.manual_seed(42 + epoch)
 
 if __name__ == '__main__':
     argp = argparse.ArgumentParser()
@@ -169,5 +258,18 @@ if __name__ == '__main__':
     
     print('test dataloader for training data')
     for epoch in range(3):
-        for i in tqdm.tqdm(loader):
-            pass
+        sampler.set_epoch(epoch)
+        air = torch.ones([214,16385], dtype=torch.complex64)
+        mic = torch.ones([69,16385], dtype=torch.complex64)
+        for x_orig, x_aug in tqdm.tqdm(loader):
+            bat = x_orig.shape[0]
+            with torch.no_grad():
+                torch.set_num_threads(4)
+                air_conv = air[torch.randint(0, 214, size=(bat,), dtype=torch.long)]
+                mic_conv = mic[torch.randint(0, 69, size=(bat,), dtype=torch.long)]
+                auga = torch.fft.rfft(x_aug, 16384*2, axis=1)
+                b = torch.fft.irfft(auga * air_conv * mic_conv, 16384*2, axis=1)
+                b = b[:,8000:16000]
+                del air_conv
+                del mic_conv
+                del auga
