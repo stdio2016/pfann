b09e6a77d6ad43c6abd04709eaa2b6d859ac3151
stdio2016 <randomfrank8@gmail.com>
2020-12-16 15:56:44 +0800

    command line argument in train.py

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
+import argparse
 import numpy as np
 from tqdm import tqdm
 import torch
 from model import FpNetwork
+from datautil.dataset import build_data_loader
 from datautil.mock_data import make_false_data
 from torch.utils.data import DataLoader
+import torch.multiprocessing as mp
 
 def similarity_loss(y, tau):
     a = torch.matmul(y, y.T)
@@ -20,7 +23,8 @@ def similarity_loss(y, tau):
 
 def train(model, optimizer, train_data, val_data, batch_size, device):
     minibatch = 40
-    train_data = DataLoader(train_data, batch_size=batch_size//2, shuffle=True)
+    if torch.cuda.get_device_properties(0).total_memory > 11e9:
+        minibatch = 640
     validate_N = val_data.shape[0] * 2
     val_data = DataLoader(val_data, batch_size=minibatch//2)
     for epoch in range(100):
@@ -28,7 +32,8 @@ def train(model, optimizer, train_data, val_data, batch_size, device):
         tau = 0.05
         print('epoch %d' % (epoch+1))
         losses = []
-        for x in tqdm(train_data):
+        pbar = tqdm(train_data)
+        for x in pbar:
             optimizer.zero_grad()
         
             x = torch.flatten(x, 0, 1)
@@ -53,7 +58,9 @@ def train(model, optimizer, train_data, val_data, batch_size, device):
                 loss = similarity_loss(y, tau)
                 loss.backward()
             optimizer.step()
-            losses.append(float(loss.item()))
+            lossnum = float(loss.item())
+            pbar.set_description('loss=%f'%lossnum)
+            losses.append(lossnum)
         print('loss: %f' % np.mean(losses))
 
         model.eval()
@@ -78,7 +85,7 @@ def train(model, optimizer, train_data, val_data, batch_size, device):
                         acc += 1
             print('validate score: %f' % (acc / validate_N))
 
-def test_train():
+def test_train(args):
     torch.manual_seed(123)
     torch.cuda.manual_seed(123)
     torch.backends.cudnn.benchmark = False
@@ -88,7 +95,7 @@ def test_train():
     u = 32
     F_bin = 256
     T = 32
-    N = 320
+    batch_size = 640
     data_N = 2000
     validate_N = 160
     device = torch.device('cuda')
@@ -97,8 +104,20 @@ def test_train():
         print('GPU mem usage: %dMB' % (torch.cuda.memory_allocated()/1024**2))
     x_mock = make_false_data(data_N, F_bin, T)
     y_mock = make_false_data(validate_N, F_bin, T)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    train(model, optimizer, x_mock, y_mock, N, device)
+    if args.csv:
+        train_data = build_data_loader(
+            csv_path=args.csv,
+            cache_dir=args.cache_dir,
+            num_workers=2, chunk_size=20000, batch_size=batch_size)
+    else:
+        train_data = DataLoader(x_mock, batch_size=batch_size//2, shuffle=True)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4 * batch_size/640)
+    train(model, optimizer, train_data, y_mock, batch_size, device)
 
 if __name__ == "__main__":
-    test_train()
+    mp.set_start_method('spawn')
+    args = argparse.ArgumentParser()
+    args.add_argument('--csv')
+    args.add_argument('--cache-dir', default='caches')
+    args = args.parse_args()
+    test_train(args)
