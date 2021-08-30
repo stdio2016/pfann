1792979973d74c2ab697821130e40112668e1bfc
stdio2016 <randomfrank8@gmail.com>
2021-08-30 18:36:16 +0800

    find threshold

--- tools/mirexacc.py
+++ tools/mirexacc.py
@@ -17,20 +17,21 @@ def extract_ans_txt(file):
             my_ans = os.path.splitext(os.path.split(ans)[1])[0]
             if my_query in out:
                 print('Warning! query %s occured twice' % query)
-            out[my_query] = my_ans
+            out[my_query] = my_ans, 0
     return out
 
 def extract_ans_csv(file):
     with open(file, 'r') as fin:
         out = {}
         reader = csv.reader(fin)
+        next(reader)
         for line in reader:
             query, ans = line[:2]
             my_query = os.path.splitext(os.path.split(query)[1])[0]
             my_ans = os.path.splitext(os.path.split(ans)[1])[0]
             if my_query in out:
                 print('Warning! query %s occured twice' % query)
-            out[my_query] = my_ans
+            out[my_query] = my_ans, float(line[2])
     return out
 
 def extract_ans(file):
@@ -43,15 +44,29 @@ PR = extract_ans(args.predict)
 
 correct = 0
 total = 0
+scores = []
 for query in PR:
-    ans = PR[query]
+    ans, sco = PR[query]
     if query in GT:
-        real_ans = GT[query]
+        real_ans, _ = GT[query]
         total += 1
         if ans == real_ans:
             correct += 1
+        scores.append((sco, ans==real_ans))
     else:
         print('query %s in prediction file not found!!' % query)
         print('ARE YOU KIDDING ME?')
         exit(1)
 print('song correct %d acc %.2f' % (correct, correct/total * 100))
+scores.sort()
+if correct == 0:
+    print('totally wrong')
+elif correct == total:
+    print('all correct')
+else:
+    thres = (scores[total-correct-1][0] + scores[total-correct][0]) / 2
+    FN = 0
+    for sco, ok in scores:
+        if sco > thres: break
+        FN += ok
+    print('threshold %f TP %d FN %d' % (thres, correct - FN, FN))
